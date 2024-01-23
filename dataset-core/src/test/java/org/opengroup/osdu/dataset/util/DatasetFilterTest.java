3c759a8763bf9bf120018ea22d41eda723a9fddb
Bruce Jin <jtyself@amazon.com>
2024-01-23 16:23:28 +0000

    Aws fix cve

--- dataset-core/src/test/java/org/opengroup/osdu/dataset/util/DatasetFilterTest.java
+++ dataset-core/src/test/java/org/opengroup/osdu/dataset/util/DatasetFilterTest.java
@@ -9,13 +9,13 @@ import org.opengroup.osdu.core.common.model.http.DpsHeaders;
 import org.springframework.mock.web.MockFilterChain;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
+import java.util.UUID;
 
 
 import javax.servlet.ServletException;
 import java.io.IOException;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DatasetFilterTest {
@@ -36,6 +36,21 @@ public class DatasetFilterTest {
         request.addHeader(FOR_HEADER_NAME,"dummy-value");
         MockHttpServletResponse response = new MockHttpServletResponse();
         MockFilterChain filterChain = new MockFilterChain();
+        when(dpsHeaders.getCorrelationId()).thenReturn(UUID.randomUUID().toString());
+        datasetFilter.doFilter(request,response,filterChain);
+        verify(dpsHeaders,times(1)).getCorrelationId();
+        verify(dpsHeaders,times(1)).addCorrelationIdIfMissing();
+    }
+
+    @Test (expected = ServletException.class)
+    public void doFilterInvalidCorrelationId()
+            throws IOException, ServletException {
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.addHeader(FOR_HEADER_NAME,"dummy-value");
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        MockFilterChain filterChain = new MockFilterChain();
+        when(dpsHeaders.getCorrelationId()).thenReturn("redirectToAPhishingURL");
         datasetFilter.doFilter(request,response,filterChain);
         verify(dpsHeaders,times(1)).getCorrelationId();
         verify(dpsHeaders,times(1)).addCorrelationIdIfMissing();
